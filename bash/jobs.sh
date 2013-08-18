26b220c46fbd41ed00c7b3cdf4b15279d1747890
Joseph Price <joseph.price@huffingtonpost.com>
2013-08-18 13:34:22 -0400

    added cleanup func

--- bash/jobs.sh
+++ bash/jobs.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env sh
 
 set -eu
+declare -a jobs
+
+cleanup() {
+  for pid in ${pids[@]}; do
+    log "killing $pid"
+    if ! kill $pid > /dev/null 2>&1; then
+      error "Did not find pid $pid"
+    fi  
+  done
+}
+
+trap "cleanup" SIGINT SIGTERM
 
 numProcessors() {
   echo $(sysctl hw.ncpu | cut -d' ' -f2)
 }
-
-declare -a jobs
 
 addJob() {
   job="$@"
